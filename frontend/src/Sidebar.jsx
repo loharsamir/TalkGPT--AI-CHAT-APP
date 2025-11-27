ef9e4a2a8e03b18d62f512460b23b4380c7092b5
“loharsamir” <samirlohar2403@gmail.com>
2025-11-27 22:56:33 +0530

    frontend deploy

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -6,7 +6,7 @@ function Sidebar(){
     const {allThreads, setAllThreads,currThreadId, setNewChat, setPrompt, setReply, setCurrThreadId, setPrevChats} = useContext(MyContext);
     const getAllThreads = async ()=>{
         try{
-            const response = await fetch("http://localhost:8080/api/thread");
+            const response = await fetch("https://talkgpt-ai-chat-app.onrender.com/api/thread");
             const res=await response.json();
             const filteredData = res.map(thread => ({threadId: thread.threadId ,title: thread.title}));
             //console.log(filteredData);
@@ -30,7 +30,7 @@ function Sidebar(){
     const changeThread = async (newThreadId) => {
         setCurrThreadId(newThreadId);
         try{
-            const response = await fetch(`http://localhost:8080/api/thread/${newThreadId}`);
+            const response = await fetch(`https://talkgpt-ai-chat-app.onrender.com/api/thread/${newThreadId}`);
             const res= await response.json();
             console.log(res);
             setPrevChats(res);
@@ -44,7 +44,7 @@ function Sidebar(){
 
     const deleteThread = async (threadId) => {
         try {
-            const response = await fetch(`http://localhost:8080/api/thread/${threadId}` , {method: "DELETE"});
+            const response = await fetch(`https://talkgpt-ai-chat-app.onrender.com/api/thread/${threadId}` , {method: "DELETE"});
             const res= await response.json();
             //updated threads re-render
             setAllThreads(prev => prev.filter(thread => thread.threadId != threadId));
